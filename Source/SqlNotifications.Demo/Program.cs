7a7ff99f9ea646732747e2331083a548475cb14f
Christian Krowiorsch <krowiorsch@landaumedia.de>
2011-10-10 14:50:37 +0200

    add Demo project

---
+++ Source/SqlNotifications.Demo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+using Krowiorsch.Dojo;
+using Krowiorsch.Dojo.Wire;
+
+namespace SqlNotifications.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = "SERVER=Localhost;DATABASE=NotificationTest;User=Guest";
+
+            var notificationTracker = Notifications.WireUp()
+                .ForDatabase(connectionString)
+                .WithNotificationsOfAssembly(Assembly.GetExecutingAssembly())
+                .PublishingTo(new NLogPublisher())
+                .Build();
+
+            notificationTracker.Start();
+
+            Console.ReadLine();
+        }
+    }
+}
