cfdfe2081c680709866fbfe93c888858b964ca82
Christian Krowiorsch <krowiorsch@landaumedia.de>
2012-01-13 12:35:15 +0100

    disable all tests (checking coverage tool)

--- Source/SqlNotifications.Tests/Version/DatabaseVersionStorageSpec.cs
+++ Source/SqlNotifications.Tests/Version/DatabaseVersionStorageSpec.cs
@@ -1,64 +1,64 @@
-﻿using LandauMedia.Storage;
-using Machine.Specifications;
+﻿//using LandauMedia.Storage;
+//using Machine.Specifications;
 
-// ReSharper disable InconsistentNaming
-// ReSharper disable UnusedMember.Local
-// ReSharper disable UnusedMember.Global
+//// ReSharper disable InconsistentNaming
+//// ReSharper disable UnusedMember.Local
+//// ReSharper disable UnusedMember.Global
 
-namespace LandauMedia.Version
-{
-    [Subject(typeof(DatabaseVersionStorage))]
-    public class When_DatabaseVersionStorage_Store_value_1_for_key_test : with_Databaseversion_Storage
-    {
-        Because of = () =>
-            _versionStorage.Store("test", 1);
+//namespace LandauMedia.Version
+//{
+//    [Subject(typeof(DatabaseVersionStorage))]
+//    public class When_DatabaseVersionStorage_Store_value_1_for_key_test : with_Databaseversion_Storage
+//    {
+//        Because of = () =>
+//            _versionStorage.Store("test", 1);
 
-        It should_return_the_value_one_for_key_test = () =>
-            _versionStorage.Load("test").ShouldEqual((ulong)1);
+//        It should_return_the_value_one_for_key_test = () =>
+//            _versionStorage.Load("test").ShouldEqual((ulong)1);
 
-        It should_handle_the_key_caseinsensitiv = () =>
-            _versionStorage.Load("TEST").ShouldEqual((ulong)1);
+//        It should_handle_the_key_caseinsensitiv = () =>
+//            _versionStorage.Load("TEST").ShouldEqual((ulong)1);
 
-        It should_return_0_for_non_existingKey = () =>
-            _versionStorage.Load("nonExistingKey").ShouldEqual((ulong)0);
+//        It should_return_0_for_non_existingKey = () =>
+//            _versionStorage.Load("nonExistingKey").ShouldEqual((ulong)0);
 
-    }
+//    }
 
-    [Subject(typeof(DatabaseVersionStorage))]
-    public class When_Store_a_existing_key : with_Databaseversion_Storage
-    {
-        Establish context = () =>
-            _versionStorage.Store("test", 5);
+//    [Subject(typeof(DatabaseVersionStorage))]
+//    public class When_Store_a_existing_key : with_Databaseversion_Storage
+//    {
+//        Establish context = () =>
+//            _versionStorage.Store("test", 5);
 
-        Because of = () =>
-            _versionStorage.Store("test", 10);
+//        Because of = () =>
+//            _versionStorage.Store("test", 10);
 
-        It should_load_the_new_key = () =>
-            _versionStorage.Load("test").ShouldEqual((ulong)10);
-    }
+//        It should_load_the_new_key = () =>
+//            _versionStorage.Load("test").ShouldEqual((ulong)10);
+//    }
 
-    [Subject(typeof(DatabaseVersionStorage))]
-    public class when_store_a_key_with_umlauts : with_Databaseversion_Storage
-    {
-        Because of = () =>
-            _versionStorage.Store("KeyWithUmlautsÄÖÜ", 1);
+//    [Subject(typeof(DatabaseVersionStorage))]
+//    public class when_store_a_key_with_umlauts : with_Databaseversion_Storage
+//    {
+//        Because of = () =>
+//            _versionStorage.Store("KeyWithUmlautsÄÖÜ", 1);
 
-        It should_load_the_value_1 = () =>
-            _versionStorage.Load("KeyWithUmlautsÄÖÜ").ShouldEqual((ulong)1);
-    }
+//        It should_load_the_value_1 = () =>
+//            _versionStorage.Load("KeyWithUmlautsÄÖÜ").ShouldEqual((ulong)1);
+//    }
 
-    public class with_Databaseversion_Storage : With_express_database
-    {
-        Establish context = () =>
-            _versionStorage = new DatabaseVersionStorage(_connectionstring);
+//    public class with_Databaseversion_Storage : With_express_database
+//    {
+//        Establish context = () =>
+//            _versionStorage = new DatabaseVersionStorage(_connectionstring);
 
-        Cleanup cleanup = () =>
-            _versionStorage.Dispose();
+//        Cleanup cleanup = () =>
+//            _versionStorage.Dispose();
 
-        protected static DatabaseVersionStorage _versionStorage;
-    }
-}
+//        protected static DatabaseVersionStorage _versionStorage;
+//    }
+//}
 
-// ReSharper restore UnusedMember.Global
-// ReSharper restore UnusedMember.Local
-// ReSharper restore InconsistentNaming
+//// ReSharper restore UnusedMember.Global
+//// ReSharper restore UnusedMember.Local
+//// ReSharper restore InconsistentNaming
--- Source/SqlNotifications/Exceptions/TableNotExistException.cs
+++ Source/SqlNotifications/Exceptions/TableNotExistException.cs
@@ -1,13 +1,37 @@
-﻿using System.Data;
+﻿using System;
+using System.Data;
+using System.Runtime.Serialization;
 
 namespace LandauMedia.Exceptions
 {
+    /// <summary>
+    /// Thrown, if the requested Table not exist on Database
+    /// </summary>
     public class TableNotExistException : DataException
     {
+        protected TableNotExistException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+
         public TableNotExistException(string tableName, string schemaName)
         {
             SchemaName = schemaName;
             TableName = tableName;
+        }
+
+        public TableNotExistException(string s)
+            : base(s)
+        {
+        }
+
+        public TableNotExistException(string s, Exception innerException)
+            : base(s, innerException)
+        {
+        }
+
+        public TableNotExistException()
+        {
         }
 
         public string SchemaName { get; set; }
