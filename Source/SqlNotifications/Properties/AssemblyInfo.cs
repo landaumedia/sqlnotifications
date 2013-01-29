3ceeb5181eaf35660ae39194ccd2ab16b02d20df
Christian Krowiorsch <krowiorsch@landaumedia.de>
2013-01-29 17:51:45 +0100

    inc version

--- Source/SqlNotifications/Properties/AssemblyInfo.cs
+++ Source/SqlNotifications/Properties/AssemblyInfo.cs
@@ -1,9 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("CommandCenter.SqlNotifications")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -13,13 +10,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("3f101967-3278-4e54-b6d8-38cc4fa7194a")]
 
-[assembly: AssemblyVersion("1.40.7")]
-[assembly: AssemblyFileVersion("1.40.7")]
+[assembly: AssemblyVersion("1.44.8")]
+[assembly: AssemblyFileVersion("1.44.8")]
