e3fc7cf85ecfcdf8cc0c5c378ad5a970cd8b0c5b
Christian Krowiorsch <krowiorsch@landaumedia.de>
2013-03-08 12:57:29 +0100

    add Tests for all Stream

--- Source/SqlNotifications.Tests/Notifications/with_StreamingNotification.cs
+++ Source/SqlNotifications.Tests/Notifications/with_StreamingNotification.cs
@@ -11,19 +11,25 @@ namespace LandauMedia.Notifications
     {
         Establish context = () =>
         {
-            _subject = new StreamingNotification<string>((id,info) => id);
+            _subject = new StreamingNotification<string>((id, info) => id);
+
+            _deletionList = new List<string>();
+            _insertList = new List<string>();
+            _updateList = new List<string>();
+            _allList = new List<string>();
+
             _subject.DeleteStream.Subscribe(s => _deletionList.Add(s));
             _subject.UpdateStream.Subscribe(s => _updateList.Add(s));
             _subject.InsertStream.Subscribe(s => _insertList.Add(s));
             _subject.Stream.Subscribe(s => _allList.Add(s));
         };
-            
+
         protected static StreamingNotification<string> _subject;
 
-        protected static List<string> _deletionList = new List<string>();
-        protected static List<string> _updateList = new List<string>();
-        protected static List<string> _insertList = new List<string>();
-        protected static List<string> _allList = new List<string>();
+        protected static List<string> _deletionList;
+        protected static List<string> _updateList;
+        protected static List<string> _insertList;
+        protected static List<string> _allList;
     }
 
     [Subject(typeof(StreamingNotification<>))]
@@ -39,6 +45,9 @@ namespace LandauMedia.Notifications
             _insertList[0].ShouldEqual("insert");
 
         It should_have_an_item_in_allstream = () =>
+            _allList.Count.ShouldEqual(1);
+
+        It should_have_an_delete_item_in_allstream = () =>
             _allList[0].ShouldEqual("insert");
     }
 
@@ -55,6 +64,9 @@ namespace LandauMedia.Notifications
             _deletionList[0].ShouldEqual("delete");
 
         It should_have_an_item_in_allstream = () =>
+            _allList.Count.ShouldEqual(1);
+
+        It should_have_an_delete_item_in_allstream = () =>
             _allList[0].ShouldEqual("delete");
     }
 
@@ -71,6 +83,32 @@ namespace LandauMedia.Notifications
             _updateList[0].ShouldEqual("update");
 
         It should_have_an_item_in_allstream = () =>
+            _allList.Count.ShouldEqual(1);
+
+        It should_have_an_delete_item_in_allstream = () =>
             _allList[0].ShouldEqual("update");
+    }
+
+
+    [Subject(typeof(StreamingNotification<>))]
+    public class when_have_multiple_events : with_StreamingNotification
+    {
+        Because of = () =>
+        {
+            _subject.OnUpdate(null, "update", null);
+            _subject.OnInsert(null, "insert", null);
+            _subject.OnDelete(null, "delete", null);
+        };
+
+        It should_have_three_items_in_allstream = () =>
+            _allList.Count.ShouldEqual(3);
+
+        It should_have_ordered_allStream = () =>
+        {
+            _allList[0].ShouldEqual("update");
+            _allList[1].ShouldEqual("insert");
+            _allList[2].ShouldEqual("delete");
+        };
+
     }
 }
